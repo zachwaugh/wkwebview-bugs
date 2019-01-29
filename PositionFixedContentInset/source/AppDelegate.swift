71bbc0892cb607a91d610f825391eee785a7be6c
Zach Waugh <zwaugh@gmail.com>
2019-01-29 17:44:12 -0500

    Update for Xcode 10.1 and Swift 4.2

--- PositionFixedContentInset/source/AppDelegate.swift
+++ PositionFixedContentInset/source/AppDelegate.swift
@@ -5,22 +5,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let viewController1 = ViewController(mode: .autoUnderlay)
-        viewController1.title = "Auto"
+        viewController1.title = "Auto Underlay"
         let navController1 = UINavigationController(rootViewController: viewController1)
 
         let viewController2 = ViewController(mode: .manualUnderlay)
-        viewController2.title = "Manual"
+        viewController2.title = "Manual Underlay"
         let navController2 = UINavigationController(rootViewController: viewController2)
 
         let viewController3 = ViewController(mode: .auto)
-        viewController3.title = "Auto 2"
+        viewController3.title = "Auto / No Underlay"
         let navController3 = UINavigationController(rootViewController: viewController3)
         navController3.navigationBar.isTranslucent = false
         
         let viewController4 = ViewController(mode: .manual)
-        viewController4.title = "Manual 2"
+        viewController4.title = "Manual / No Underlay"
         let navController4 = UINavigationController(rootViewController: viewController4)
         navController4.navigationBar.isTranslucent = false
 
